4408410bde923814b0690c0c220cea7a4b2edc51
sasan-ebrahimi <sasan.ebrahimi1001@gmail.com>
2020-10-19 23:35:56 +0330

    Initial

---
+++ paircolorview/src/main/java/com/sasanebrahimi/paircolorview/PairColorView.kt
@@ -0,0 +1,58 @@
+package com.sasanebrahimi.paircolorview
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class PairColorView : View {
+
+    private var darkColor: Int = Color.GREEN
+    private var lightColor: Int = Color.CYAN
+
+    private lateinit var darkPaint: Paint
+    private lateinit var lightPaint: Paint
+
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context ,attrs) {
+        initialize(context,attrs)
+    }
+
+    fun initialize(context: Context){
+        //initialize(context,null)
+    }
+
+    fun initialize(context: Context, attrs: AttributeSet){
+        initialize(context)
+        context.obtainStyledAttributes(attrs,
+            R.styleable.PairColorView, 0, 0).apply {
+            try {
+                darkColor = getColor(R.styleable.PairColorView_darkColor, Color.GREEN)
+                lightColor = getColor(R.styleable.PairColorView_lightColor, Color.CYAN)
+            } finally {
+                recycle()
+            }
+        }
+
+        darkPaint = Paint().apply {
+            color = darkColor
+        }
+        lightPaint = Paint().apply {
+            color = lightColor
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawCircle(width/2.toFloat() , height/2.toFloat(), width.toFloat()/2 , darkPaint)
+
+
+        canvas?.drawArc(
+            RectF(
+            0f, 0f ,width.toFloat(),height.toFloat()),0f,180f, true,lightPaint)
+    }
+
+}
